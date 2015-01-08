606991c37b2095198d179b391dbd7eb1d538b9f6
Eric Müller <mueller@kip.uni-heidelberg.de>
2015-01-08 15:02:59 +0100

    add HBP logo for make latexpdf

--- conf.py
+++ conf.py
@@ -208,7 +208,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'HBP_Primary_RGB_logoOnly.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
