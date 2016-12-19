41e15222028ed9106011165a29a1255a103ce00d
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2016-12-19 12:54:05 +0100

    NMPM-1: update single neuron example

--- pm/examples/nmpm1_single_neuron.py
+++ pm/examples/nmpm1_single_neuron.py
@@ -1,110 +1,158 @@
-# -*- coding: utf-8 -*-
-#
-# Minimal Working Example of a single neuron and a single spike source on NMPM1
-#
-# Andreas Stöckel, October 2015
-#
-# Modified by S. Schmitt, October 2015
-# Modified by S. Schmitt, July 2016
+#!/usr/bin/python
+# -*- coding: utf-8; -*-
 
 import numpy as np
-import pylogging
+
+from pyhalbe import HICANN
+import pyhalbe.Coordinate as C
+from pysthal.command_line_util import init_logger
+
 import pyhmf as pynn
-from pymarocco import PyMarocco
-from pyhalbe.Coordinate import Wafer, HICANNOnWafer, Enum
+from pymarocco import PyMarocco, Defects
+from pymarocco.runtime import Runtime
+from pymarocco.coordinates import LogicalNeuron
+from pymarocco.results import Marocco
 
-# Number of hardware neurons used for a logical neuron
-neuron_size = 4
+init_logger("WARN", [
+    ("guidebook", "DEBUG"),
+    #("marocco", "TRACE"),
+    #("calibtic", "DEBUG"),
+])
 
-# Excitatory neuron weight
-w_exc = 0.1
+import pylogging
+logger = pylogging.get("guidebook")
 
-# IF_cond_exp parameters
-params = {
-    'v_reset': -70,
-    'v_rest': -50,
-    'v_thresh': -45,
-    'e_rev_I': -70,
-    'e_rev_E': 10,
-    'tau_m': 16,
-    'tau_syn_E': 17,
-    'tau_syn_I': 17,
+neuron_parameters = {
+    'cm': 0.2,
+    'v_reset': -70.,
+    'v_rest': -20.,
+    'v_thresh': -10,
+    'e_rev_I': -100.,
+    'e_rev_E': 60.,
+    'tau_m': 20.,
+    'tau_refrac': 0.1,
+    'tau_syn_E': 5.,
+    'tau_syn_I': 5.,
 }
 
-# Deactivate logging -- not doing so causes multi-megabyte logfiles which fail
-# to be transfered back to the Neuromorphic Compute Platform, causing the job
-# to indefenitely hang in the "running" state
-for domain in ["Default", "marocco", "sthal.HICANNConfigurator.Time", "hicann-system", "halbe"]:
-    pylogging.set_loglevel(pylogging.get(domain), pylogging.LogLevel.ERROR)
-
-# but we need a little bit of output to know what's going on
-pylogging.set_loglevel(pylogging.get("Calibtic"), pylogging.LogLevel.DEBUG)
-pylogging.set_loglevel(pylogging.get("marocco"), pylogging.LogLevel.DEBUG)
-
-# Setup the router/placement engine
 marocco = PyMarocco()
-marocco.neuron_placement.default_neuron_size(neuron_size)
-
-# for backend Hardware
-# temporary method to allow locking of synapse drivers by using
-# the background generators also for FPGA input routes
-marocco.neuron_placement.restrict_rightmost_neuron_blocks(True)
-
+marocco.neuron_placement.default_neuron_size(4)
 marocco.neuron_placement.minimize_number_of_sending_repeaters(False)
+marocco.merger_routing.strategy(marocco.merger_routing.one_to_one)
+
+marocco.bkg_gen_isi = 125
+marocco.pll_freq = 125e6
+
 marocco.backend = PyMarocco.Hardware
 marocco.calib_backend = PyMarocco.XML
-marocco.calib_path = "/wang/data/calibration/review_2016"
-marocco.persist = "results.bin"
+marocco.defects.path = marocco.calib_path = "/wang/data/calibration/ITL_2016"
+marocco.defects.backend = Defects.XML
+marocco.default_wafer = C.Wafer(33)
+marocco.param_trafo.use_big_capacitors = True
+marocco.input_placement.consider_firing_rate(True)
+marocco.input_placement.bandwidth_utilization(0.8)
+
+runtime = Runtime(marocco.default_wafer)
+pynn.setup(marocco=marocco, marocco_runtime=runtime)
+
+#  ——— set up network ——————————————————————————————————————————————————————————
+
+pop = pynn.Population(1, pynn.IF_cond_exp, neuron_parameters)
+
+pop.record()
+pop.record_v()
+
+hicann = C.HICANNOnWafer(C.Enum(367))
+marocco.manual_placement.on_hicann(pop, hicann)
+
+connector = pynn.AllToAllConnector(weights=1)
+
+exc_spike_times = [
+    250,
+    500,
+    520,
+    540,
+    1250,
+]
+
+inh_spike_times = [
+    750,
+    1000,
+    1020,
+    1040,
+    1250,
+]
+
+duration = 1500.0
+
+stimulus_exc = pynn.Population(1, pynn.SpikeSourceArray, {
+    'spike_times': exc_spike_times})
+stimulus_inh = pynn.Population(1, pynn.SpikeSourceArray, {
+    'spike_times': inh_spike_times})
+
+projections = [
+    pynn.Projection(stimulus_exc, pop, connector, target='excitatory'),
+    pynn.Projection(stimulus_inh, pop, connector, target='inhibitory'),
+]
+
+#  ——— run mapping —————————————————————————————————————————————————————————————
+
+marocco.skip_mapping = False
+marocco.backend = PyMarocco.None
+
+pynn.reset()
+pynn.run(duration)
+
+#  ——— change low-level parameters before configuring hardware —————————————————
+
+def set_sthal_params(wafer, gmax, gmax_div):
+    for hicann in wafer.getAllocatedHicannCoordinates():
+        fgs = wafer[hicann].floating_gates
+        for ii in xrange(fgs.getNoProgrammingPasses()):
+            cfg = fgs.getFGConfig(C.Enum(ii))
+            cfg.fg_biasn = 0
+            cfg.fg_bias = 0
+            fgs.setFGConfig(C.Enum(ii), cfg)
+
+        for block in C.iter_all(C.FGBlockOnHICANN):
+            fgs.setShared(block, HICANN.shared_parameter.V_gmax0, gmax)
+            fgs.setShared(block, HICANN.shared_parameter.V_gmax1, gmax)
+            fgs.setShared(block, HICANN.shared_parameter.V_gmax2, gmax)
+            fgs.setShared(block, HICANN.shared_parameter.V_gmax3, gmax)
+
+        for block in C.iter_all(C.FGBlockOnHICANN):
+            fgs.setShared(block, HICANN.shared_parameter.V_dllres, 275)
+            fgs.setShared(block, HICANN.shared_parameter.V_ccas, 800)
+
+        for driver in C.iter_all(C.SynapseDriverOnHICANN):
+            for row in C.iter_all(C.RowOnSynapseDriver):
+                wafer[hicann].synapses[driver][row].set_gmax_div(
+                    C.left, gmax_div)
+                wafer[hicann].synapses[driver][row].set_gmax_div(
+                    C.right, gmax_div)
+
+set_sthal_params(runtime.wafer(), gmax=1023, gmax_div=1)
+
+#  ——— configure hardware ——————————————————————————————————————————————————————
+
+marocco.skip_mapping = True
+marocco.backend = PyMarocco.Hardware
+# Full configuration during first step
 marocco.hicann_configurator = PyMarocco.HICANNv4Configurator
 
-marocco.default_wafer = Wafer(33)
+for digital_weight in [5, 10, 15]:
+    logger.info("running measurement with digital weight {}".format(digital_weight))
+    for proj in projections:
+        proj_item, = runtime.results().synapse_routing.synapses().find(proj)
+        synapse = proj_item.hardware_synapse()
 
-# choose membrane capacitance
-# marocco.param_trafo.use_big_capacitors = False
+        proxy = runtime.wafer()[synapse.toHICANNOnWafer()].synapses[synapse]
+        proxy.weight = HICANN.SynapseWeight(digital_weight)
 
-use=HICANNOnWafer(Enum(367)) # choose hicann
-marocco.analog_enum = 0
-marocco.hicann_enum = use.id().value()
+    pynn.run(duration)
+    np.savetxt("membrane_w{}.txt".format(digital_weight), pop.get_v())
+    np.savetxt("spikes_w{}.txt".format(digital_weight), pop.getSpikes())
+    pynn.reset()
 
-#output
-#marocco.membrane = "membrane.dat" # voltage trace
-marocco.wafer_cfg = "wafer.bin"   # configuration
-marocco.roqt = "example.roqt"     # visualization
-
-#example for blacklisting, one pyredman.Hicann instance per HICANN
-#h = pyredman.Hicann()
-#h.drivers().disable(SynapseDriverOnHICANN(C.right, C.Y(4)))
-#h.neurons().disable(NeuronOnHICANN(Enum(0)))
-#marocco.defects.inject(use, h)
-
-# Setup the NMPM1 PyNN interface "pyhmf"
-pynn.setup(marocco = marocco)
-
-# Create the spike source -- generate a spike all 100ms for 10s
-spike_times = range(0, 10000, 100)
-in_0 = pynn.Population(5, pynn.SpikeSourceArray, {
-    'spike_times': spike_times
-})
-
-# Create the target neuron
-nn_0 = pynn.Population(1, pynn.IF_cond_exp, params)
-
-# Introduce the neuron to the placement engine
-marocco.manual_placement.on_hicann(nn_0, use)
-
-# Add a connection between the spike source in_0 and the neuron nn_0 -- the
-# connection needs to be added neuron_size times
-for _ in xrange(neuron_size):
-    pynn.Projection(in_0, nn_0,
-        pynn.FromListConnector([(0, 0, w_exc, 0.0)]), target = 'excitatory')
-
-# Run the simulation, continue running for 1s after the last spike
-pynn.run(max(spike_times) + 1000.0)
-
-# End the simulation, gather results
-pynn.end()
-
-# Fetch the spike times and print them
-spikes = nn_0.getSpikes()
-print ">>>>", spikes
-np.savetxt("spikes.dat", spikes)
+    # only change digital parameters from now on
+    marocco.hicann_configurator = PyMarocco.NoResetNoFGConfigurator
