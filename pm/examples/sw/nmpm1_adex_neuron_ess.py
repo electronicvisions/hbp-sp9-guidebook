41fd4fdae1afebfeeb8f813df7b2312790e719d7
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2018-04-16 16:40:29 +0200

    NMPM-1: use HICANNConfigurator for ESS example

--- pm/examples/sw/nmpm1_adex_neuron_ess.py
+++ pm/examples/sw/nmpm1_adex_neuron_ess.py
@@ -23,6 +23,7 @@ marocco = PyMarocco()
 marocco.backend = PyMarocco.ESS # choose Executable System Specification instead of real hardware
 marocco.calib_backend = PyMarocco.CalibBackend.Default
 marocco.defects.backend = Defects.Backend.None
+marocco.hicann_configurator = PyMarocco.HICANNConfigurator
 marocco.experiment_time_offset = 5.e-7 # can be low for ESS, as no repeater locking required
 marocco.neuron_placement.default_neuron_size(4) # default number of hardware neuron circuits per pyNN neuron
 marocco.persist = "nmpm1_adex_neuron_ess.bin"
